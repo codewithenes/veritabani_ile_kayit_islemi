f51e8bf11e04b7eb98be291f5e32f85e5cf380ec
codewithenes <53350986+codewithenes@users.noreply.github.com>
2020-06-06 19:58:22 +0300

    Add files via upload

---
+++ deneme1/deneme1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Drawing.Text;
+
+namespace deneme1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=bilgiler.mdb");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text =="Ekle")
+            {
+                baglanti.Open();
+                OleDbCommand komut = new OleDbCommand("insert into kayitbilgileri(id,ad,soyad,adres,telefon,mail) values('" + textBox7.Text + "','" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "')", baglanti);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Başarıyla Eklendi", "Kayıt");
+                tablo.Clear();
+                listele();
+                for (int i = 0; i < Controls.Count; i++)
+                {
+                    if (Controls[i] is TextBox)
+                    {
+                        Controls[i].Text = "";
+                    }
+                }
+            }
+             if(button1.Text =="Güncelle")
+            {
+                baglanti.Open();
+                OleDbCommand komut2 = new OleDbCommand("update kayitbilgileri set ad='"+textBox1.Text+ "',soyad='" + textBox2.Text + "',adres='" + textBox3.Text + "',telefon='" + textBox4.Text + "',mail='" + textBox5.Text +"' where id = '"+textBox7.Text+"'", baglanti);
+                komut2.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Başarıyla Güncellendi Eğer Güncellenmedi ise Kaydı silip Tekrar Oluşturun", "Kayıt");
+                tablo.Clear();
+                listele();
+                for (int i = 0; i < Controls.Count; i++)
+                {
+                    if (Controls[i] is TextBox)
+                    {
+                        Controls[i].Text = "";
+                    }
+                }
+                button1.Text="Ekle";
+            }
+        }
+        DataTable tablo = new DataTable();
+
+        private void listele()
+        {
+            baglanti.Open();
+            OleDbDataAdapter adtr=new OleDbDataAdapter("select *from kayitbilgileri", baglanti);
+            adtr.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("delete * from kayitbilgileri where ad='"+dataGridView1.CurrentRow.Cells["ad"].Value.ToString()+"' and id='" + dataGridView1.CurrentRow.Cells["id"].Value.ToString() + "'  " , baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt Başarıyla Silindi", "Kayıt");
+            tablo.Clear();
+            listele();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbDataAdapter adtr = new OleDbDataAdapter("select *from kayitbilgileri where ad like '"+textBox6.Text+"%' or id like '"+textBox6.Text+ "%'", baglanti);
+            DataTable tablo2 = new DataTable();
+            adtr.Fill(tablo2);
+            dataGridView1.DataSource = tablo2;
+            baglanti.Close();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox7.Text = dataGridView1.CurrentRow.Cells["id"].Value.ToString();
+            textBox1.Text = dataGridView1.CurrentRow.Cells["ad"].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells["soyad"].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells["adres"].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells["telefon"].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells["mail"].Value.ToString();
+            
+            button1.Text= "Güncelle";
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+ 
+           
+        }
+
+        private void textBox7_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ deneme1/deneme1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("deneme1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("deneme1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f999b2c6-0fb1-408c-8386-281a1a961c6b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
